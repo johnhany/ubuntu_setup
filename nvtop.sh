874f7437a0edd06abf8e4a444dc1d0cf5f393555
John Hany <johnhany@163.com>
2019-05-02 21:10:10 +0800

    system-monitor & nvtop

---
+++ nvtop.sh
@@ -0,0 +1,14 @@
+sudo apt install cmake libncurses5-dev libncursesw5-dev git
+
+cd ~/Downloads
+
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+
+# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
+# try the following command instead, otherwise skip to the build with make.
+# cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+
+make
+sudo make install
---
+++ system-monitor.sh
@@ -0,0 +1,3 @@
+sudo apt-get install chrome-gnome-shell
+sudo apt-get install gir1.2-gtop-2.0 gir1.2-networkmanager-1.0  gir1.2-clutter-1.0
+# https://extensions.gnome.org/extension/120/system-monitor/
