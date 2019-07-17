dd017ab9670b73ea2547b747f49c00c1781080e2
John Hany <johnhany@163.com>
2019-07-17 23:26:31 +0800

    changed to pytorch-cuda10

--- conda.sh
+++ conda.sh
@@ -6,4 +6,6 @@ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 conda create -n torch python=3.7
 conda activate torch
 conda install cudatoolkit=10.0
-pip install torch torchvision numpy scipy matplotlib opencv-python
+pip install numpy scipy matplotlib opencv-python
+pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-linux_x86_64.whl
+pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl
