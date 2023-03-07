f073c64da730a74a2fbf4be9fb35721690696a3f
John Hany <johnhany@163.com>
2023-03-07 10:46:10 +0800

    update cmake

--- cmake.sh
+++ cmake.sh
@@ -1,13 +1,15 @@
 sudo apt remove --purge --auto-remove cmake
 
-version=3.22
-build=1
+sudo apt-get install libssl-dev mesa-utils freeglut3-dev
+
+version=3.25
+build=2
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
 
 ./bootstrap
-make -j4
-sudo make install
+gmake -j8
+sudo gmake install
 
 cmake --version
